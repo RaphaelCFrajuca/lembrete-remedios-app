3265597f96a8631714c3db4bdf144e27611d466a
Raphael Frajuca <raphael.frajuca@solucx.com.br>
2023-06-14 03:09:42 -0300

    Usar useEffect para registrar usuários

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Layout, Menu, Spin } from "antd";
 import { UnorderedListOutlined, PlusOutlined } from "@ant-design/icons";
 import "./App.css";
@@ -10,82 +10,88 @@ import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
 
 const App: React.FC = () => {
-    const [selectedMenuItem, setSelectedMenuItem] = useState<string | null>(null);
-    const [userRegistered, setUserRegistered] = useState<boolean>(false);
-    const { isAuthenticated, isLoading, loginWithRedirect, user, getIdTokenClaims } = useAuth0();
-    const { Content, Footer, Sider } = Layout;
-    const apiUrl = process.env.REACT_APP_API_URL;
+  const [selectedMenuItem, setSelectedMenuItem] = useState<string | null>(null);
+  const [userRegistered, setUserRegistered] = useState<boolean>(false);
+  const { isAuthenticated, isLoading, loginWithRedirect, user, getIdTokenClaims } = useAuth0();
+  const { Content, Footer, Sider } = Layout;
+  const apiUrl = process.env.REACT_APP_API_URL;
 
-    const handleMenuClick = (e: any) => {
-        setSelectedMenuItem(e.key);
-        if (e.key === "1") {
-            history.push("/reminder/new");
-        } else if (e.key === "2") {
-            history.push("/reminder");
-        }
+  const handleMenuClick = (e: any) => {
+    setSelectedMenuItem(e.key);
+    if (e.key === "1") {
+      history.push("/reminder/new");
+    } else if (e.key === "2") {
+      history.push("/reminder");
+    }
+  };
+
+  useEffect(() => {
+    const registerUser = async () => {
+      const userToken = await getIdTokenClaims();
+      if (!userToken) return;
+
+      try {
+        await axios.get(`${apiUrl}/user/find`, { headers: { Authorization: `Bearer ${userToken.__raw}` }, params: { email: user?.email } });
+        setUserRegistered(true);
+      } catch (error) {
+        axios.post(`${apiUrl}/user/register`, user, { headers: { Authorization: `Bearer ${userToken.__raw}` }});
+        setUserRegistered(true);
+      }
+
+      if (userRegistered) {
+        await axios.post(`${apiUrl}/user/update`, user, { headers: { Authorization: `Bearer ${userToken.__raw}` } });
+      }
     };
 
-    if (isLoading) {
-        return (
-            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-                <Spin size="large" />
-            </div>
-        );
-    }
+    registerUser();
+  }, [getIdTokenClaims, apiUrl, user, userRegistered]);
 
-    if (!isAuthenticated) {
-        loginWithRedirect();
-    } else {
-        getIdTokenClaims().then(async user => {
-            await axios
-                .get(`${apiUrl}/user/find`, { headers: { Authorization: `Bearer ${user?.__raw}` }, params: { email: user?.email } })
-                .then(response => {
-                    setUserRegistered(true);
-                })
-                .catch(error => {
-                    axios.post(`${apiUrl}/user/register`, user, { headers: { Authorization: `Bearer ${user?.__raw}` }});
-                    setUserRegistered(true);
-                });
-            if (userRegistered) {
-                await axios.post(`${apiUrl}/user/update`, user, { headers: { Authorization: `Bearer ${user?.__raw}` } });
-            }
-        });
-    }
+  const handleLogoClick = () => {
+    setSelectedMenuItem(null);
+    history.push("/");
+  };
 
-    const handleLogoClick = () => {
-        setSelectedMenuItem(null);
-        history.push("/");
-    };
-
+  if (isLoading) {
     return (
-        <Router history={history}>
-            <Layout style={{ minHeight: "100vh", padding: 0 }}>
-                <Sider breakpoint="lg" collapsedWidth="0">
-                    <div className="logo" onClick={handleLogoClick} style={{ cursor: "pointer" }} />
-                    <Menu theme="dark" mode="inline" selectedKeys={[selectedMenuItem as string]}>
-                        <Menu.Item key="1" icon={<PlusOutlined />} onClick={handleMenuClick}>
-                            Agendar Lembretes
-                        </Menu.Item>
-                        <Menu.Item key="2" icon={<UnorderedListOutlined />} onClick={handleMenuClick}>
-                            Meus Lembretes
-                        </Menu.Item>
-                    </Menu>
-                </Sider>
-                <Layout>
-                    <Content style={{ margin: "24px 16px 0", borderRadius: 20, overflow: "hidden" }}>
-                        <div style={{ background: "#fff", minHeight: 36 }}>
-                            <Switch>
-                                <Route exact path="/" component={ReminderComponent} />
-                                {isAuthenticated && <Route path="/reminder/new" component={RegisterReminderComponent} />}
-                                {isAuthenticated && <Route path="/reminder" component={ReminderComponent} />}
-                            </Switch>
-                        </div>
-                    </Content>
-                    <Footer style={{ textAlign: "center" }}>© 2023 Raphael Carneiro Frajuca. Todos os direitos reservados.</Footer>
-                </Layout>
-            </Layout>
-        </Router>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+        <Spin size="large" />
+      </div>
     );
+  }
+
+  if (!isAuthenticated) {
+    loginWithRedirect();
+  }
+
+  return (
+    <Router history={history}>
+      <Layout style={{ minHeight: "100vh", padding: 0 }}>
+        <Sider breakpoint="lg" collapsedWidth="0">
+          <div className="logo" onClick={handleLogoClick} style={{ cursor: "pointer" }} />
+          <Menu theme="dark" mode="inline" selectedKeys={[selectedMenuItem as string]}>
+            <Menu.Item key="1" icon={<PlusOutlined />} onClick={handleMenuClick}>
+              Agendar Lembretes
+            </Menu.Item>
+            <Menu.Item key="2" icon={<UnorderedListOutlined />} onClick={handleMenuClick}>
+              Meus Lembretes
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Content style={{ margin: "24px 16px 0", borderRadius: 20, overflow: "hidden" }}>
+            <div style={{ background: "#fff", minHeight: 36 }}>
+              <Switch>
+                <Route exact path="/" component={ReminderComponent} />
+                {isAuthenticated && <Route path="/reminder/new" component={RegisterReminderComponent} />}
+                {isAuthenticated && <Route path="/reminder" component={ReminderComponent} />}
+              </Switch>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>© 2023 Raphael Carneiro Frajuca. Todos os direitos reservados.</Footer>
+        </Layout>
+      </Layout>
+    </Router>
+  );
 };
 
 export default App;
